d5725480efdc1c18bec4c9e25bb528e439608b00
Zainab <zseriki110@gmail.com>
2025-09-30 20:03:51 -0500

    This is my first commit

--- pandasPractice.py
+++ pandasPractice.py
@@ -4,3 +4,4 @@ import pandas as pd
 employee_names = ['Amy White', 'Jack Stewart', 'Richard Lauderdale', 'Sara Johnson']
 employee_names_Series = pd.Series(employee_names)
 print(employee_names_Series)
+print(type(employee_names_Series))
