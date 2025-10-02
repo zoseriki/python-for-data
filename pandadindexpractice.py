9e89f1dcca97087191a95df00518f3607f9e9998
Zainab <zseriki110@gmail.com>
2025-10-01 20:57:35 -0500

    This is my first commit

---
+++ pandadindexpractice.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+workers_age = {'Amy White':50, 'Jack Stewart':53, 'Richard Lauderdale':35, 'Sara Johnson':43}
+workers_age = pd.Series(workers_age)
+print(pd.Series(workers_age).index)
