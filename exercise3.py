253e15fddc0791ff9d9d930fc2bc531e5712ce10
Zainab <zseriki110@gmail.com>
2025-10-02 09:45:24 -0500

    This is my first commit

---
+++ exercise3.py
@@ -0,0 +1,20 @@
+#This is a practice code on how to use the .head() method in pandas.
+
+import pandas as pd
+
+employees_work_exp = pd.Series({
+'Amy White'   : 3,
+'Jack Stewart'   : 5,
+'Richard Lauderdale'  : 4.5,
+'Sara Johnson'  : 22,
+'Patrick Adams' : 28,
+'Jessica Baker'  : 14,
+'Peter Hunt'   : 4,
+'Daniel Lloyd'  : 6,
+'John Owen'   : 1.5,
+'Jennifer Phillips'  : 10,
+'Courtney Rogers'   : 4.5,
+'Anne Robinson'  : 2,
+})
+
+print(employees_work_exp.head())
--- pandasIndexpractice.py
+++ pandasIndexpractice.py
@@ -1,3 +1,5 @@
+#This is a practice code on how to use index in pandas series
+
 import pandas as pd
 
 workers_age = {'Amy White':50, 'Jack Stewart':53, 'Richard Lauderdale':35, 'Sara Johnson':43}
